3a2ab01045ee1da9d4d2ed907da2cd6ba53cec86
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-05-10 20:42:56 +0200

    added instructions

--- src/components/units/Button/Button.js
+++ src/components/units/Button/Button.js
@@ -5,6 +5,11 @@ import {faUser} from '@fortawesome/free-solid-svg-icons';
 // Styles
 import {ButtonStyled} from './Button.styles';
 
+// 1. Make the icon available from the parent component
+// 2. Programatically style the icon
+// 3. Programatically choose the position
+// 4. To show a spinner while it's loading
+
 function Button({text, loadingText, isLoading = false, onClick}) {
 	const handleClick = () => {
 		if (isLoading) {
