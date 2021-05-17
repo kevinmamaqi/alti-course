5ed8419113b4dca177e76e64d769df77f20719cc
Kevin Mamaqi <kevinmamaqi@gmail.com>
2021-05-17 21:11:14 +0200

    NAVIGATION

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import 'normalize.css';
 import 'assets/css/global.css';
 import Home from 'screens/Home/Home';
+import Contact from 'screens/Contact/Contact';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
-	return <Home />;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/contact/:id" component={Contact} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
