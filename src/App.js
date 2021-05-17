ba489f733bc7ce40aaecb555b5f638311cfc2d38
Stefano MARZULLO <stefano.marzullo@alten.it>
2021-05-17 21:12:43 +0200

    install react router dom

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import 'normalize.css';
 import 'assets/css/global.css';
 import Home from 'screens/Home/Home';
 
-function App() {
-	return <Home />;
-}
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+function App() {
+  return (
+    <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
+    </Router>
+  );
+}
 export default App;
